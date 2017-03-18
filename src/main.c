37fb6f6dd86be73534c106c18d90d2da2b6e9cce
Kevin L. Mitchell <klmitch@mit.edu>
2017-03-18 15:32:41 -0500

    Begin creating argument processing
    This commit adds the beginnings of argument processing to humboldt, using
    getopt_long() (with AX_FUNC_GETOPT_LONG to paper over incompatibilities).

--- src/main.c
+++ src/main.c
@@ -14,12 +14,60 @@
 ** permissions and limitations under the License.
 */
 
+#include <config.h>
+
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Recognized short options */
+static const char *opt_str = "c:d";
+
+/* Recognized long options */
+static const struct option opts[] = {
+  {"config", required_argument, 0, 'c'},
+  {"debug", no_argument, 0, 'd'},
+  {0, 0, 0, 0}
+};
+
+static void
+parse_args(int argc, char **argv)
+{
+  int c;
+
+  while ((c = getopt_long(argc, argv, opt_str, opts, 0)) >= 0)
+    switch (c) {
+    case 'c':
+      printf("Option -%c: Use configuration file \"%s\"\n", c, optarg);
+      break;
+
+    case 'd':
+      printf("Option -%c: Debugging enabled\n", c);
+      break;
+
+    case '?':
+      fprintf(stderr, "Bad option; usage:\n");
+      exit(EXIT_FAILURE);
+      break;
+
+    default:
+      fprintf(stderr, "Unimplemented option -%c\n", c);
+      break;
+    }
+
+  /* Check for any unrecognized arguments */
+  if (argv[optind]) {
+    fprintf(stderr, "Extraneous trailing arguments; usage:\n");
+    exit(EXIT_FAILURE);
+  }
+}
 
 int
 main(int argc, char **argv)
 {
   printf("Hello, world!\n");
+
+  parse_args(argc, argv);
 
   return 0;
 }
