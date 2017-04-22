03229102d88aba8740c9831d2654de2e8e6b2c60
Kevin L. Mitchell <klmitch@mit.edu>
2017-04-22 12:41:41 -0500

    Teach protocol analyzer about SASL messages
    This change introduces the necessary support for the Humboldt
    Protocol Analyzer to understand SASL messages.

--- tools/hum_proto/hum_proto/message.py
+++ tools/hum_proto/hum_proto/message.py
@@ -1017,3 +1017,335 @@ def _protocol2(**kwargs):
         return StartTLSReply(**kwargs)
 
     return StartTLSRequest(**kwargs)
+
+
+class SASLError(Message):
+    """
+    Represent a SASL error.
+    """
+
+    PROTOCOL = 3
+    default_carrier_flags = 'error'
+
+    # Attributes specific to this message type to display
+    MSG_ATTRS = collections.OrderedDict([
+        ('msg', six.text_type),
+    ])
+
+    @classmethod
+    def _decode(cls, **kwargs):
+        """
+        Decode a SASL error message.
+
+        :param bytes payload: The SASL error message.
+        :param **kwargs: Other keyword arguments to pass to the
+                         constructor.
+
+        :returns: An appropriately initialized instance of the SASL
+                  error message class.
+        :rtype: ``SASLError``
+        """
+
+        # Extract the message
+        msg = kwargs['payload'].decode('utf-8')
+
+        return cls(msg, **kwargs)
+
+    def __init__(self, msg, **kwargs):
+        """
+        Initialize a ``SASLError`` instance.
+
+        :param msg: The error message.
+        :param **kwargs: Additional keyword arguments to use for
+                         initializing the ``Message`` portion.
+        """
+
+        # Initialize the Message
+        super(SASLError, self).__init__(**kwargs)
+
+        # Save the data
+        self.msg = msg
+
+    def _encode(self):
+        """
+        Assemble the payload.  The payload will be appended to the carrier
+        protocol header to produce a complete protocol message.
+
+        :returns: The assembled payload.
+        :rtype: ``bytes``
+        """
+
+        return self.msg.encode('utf-8')
+
+    @attrs.FilterAttr
+    def msg(self, msg):
+        """
+        The error message.  This will be a string.
+        """
+
+        # Ensure the message is a string
+        if not isinstance(msg, six.string_types):
+            raise ValueError('Message must be a string')
+
+        return msg
+
+
+class RequestSASLMechanisms(Message):
+    """
+    Request a list of available SASL mechanisms.
+    """
+
+    PROTOCOL = 3
+
+    @classmethod
+    def _decode(cls, mechlen, **kwargs):
+        """
+        Decode a SASL mechanism list request.
+
+        :param int mechlen: The size of the mechanism name.  This will
+                            always be 255 and is ignored.
+        :param **kwargs: Other keyword arguments to pass to the
+                         constructor.
+
+        :returns: An appropriately initialized instance of the SASL
+                  mechanism list request.
+        :rtype: ``RequestSASLMechanisms``
+        """
+
+        return cls(**kwargs)
+
+    def _encode(self):
+        """
+        Assemble the payload.  The payload will be appended to the carrier
+        protocol header to produce a complete protocol message.
+
+        :returns: The assembled payload.
+        :rtype: ``bytes``
+        """
+
+        return b'\xff'
+
+
+class SASLMechanisms(Message):
+    """
+    Represent a list of SASL mechanisms.
+    """
+
+    PROTOCOL = 3
+    default_carrier_flags = 'reply'
+
+    # Attributes specific to this message type to display
+    MSG_ATTRS = collections.OrderedDict([
+        ('mechs', _splitter),
+    ])
+
+    @classmethod
+    def _decode(cls, mechlen, **kwargs):
+        """
+        Decode a SASL mechanism list.
+
+        :param int mechlen: The size of the mechanism name.  This will
+                            always be 255 and is ignored.
+        :param **kwargs: Other keyword arguments to pass to the
+                         constructor.
+
+        :returns: An appropriately initialized instance of the SASL
+                  mechanism list.
+        :rtype: ``SASLMechanisms``
+        """
+
+        # Split the list of mechanisms
+        mechs = kwargs['payload'][1:].split()
+
+        return cls(mechs, **kwargs)
+
+    def __init__(self, mechs, **kwargs):
+        """
+        Initialize a ``SASLMechanisms`` instance.
+
+        :param mechs: A list of mechanism names, expressed as
+                      ``bytes`` objects.
+        """
+
+        # Initialize the Message
+        super(SASLMechanisms, self).__init__(**kwargs)
+
+        # Save the data
+        self.mechs = mechs
+
+    def _encode(self):
+        """
+        Assemble the payload.  The payload will be appended to the carrier
+        protocol header to produce a complete protocol message.
+
+        :returns: The assembled payload.
+        :rtype: ``bytes``
+        """
+
+        return b'\xff' + b' '.join(self.mechs)
+
+    @attrs.FilterAttr
+    def mechs(self, mechs):
+        """
+        The mechanisms.  This will be a list of ``bytes`` objects.
+        """
+
+        # Ensure the message is a list of bytes objects
+        if not isinstance(mechs, list) or not all(
+                isinstance(mech, bytes) for mech in mechs
+        ):
+            raise ValueError('Mechanisms list must be a list of bytes objects')
+
+        # Uppercase the mechanims, for convenience
+        return [m.upper() for m in mechs]
+
+
+class SASLClientStep(Message):
+    """
+    Represent the client step in a SASL exchange.
+    """
+
+    PROTOCOL = 3
+
+    # Struct for collecting the mechanism name length
+    _mechlen = struct.Struct('>B')
+
+    # Attributes specific to this message type to display
+    MSG_ATTRS = collections.OrderedDict([
+        ('mech', _byter),
+        ('data', _byter),
+    ])
+
+    @classmethod
+    def _decode(cls, mechlen, **kwargs):
+        """
+        Decode a SASL client step.
+
+        :param int mechlen: The length of the mechanism name.
+        :param bytes payload: The message payload, containing the
+                              mechanism length, the mechanism name,
+                              and the client data.
+        :param **kwargs: Other keyword arguments to pass to the
+                         constructor.
+
+        :returns: An appropriately initialized instance of the SASL
+                  client step class.
+        :rtype: ``SASLClientStep``
+        """
+
+        # Extract the mechanism name, if any
+        if mechlen:
+            mech_end = cls._mechlen.size + mechlen
+            mech = kwargs['payload'][cls._mechlen.size:mech_end]
+        else:
+            mech_end = cls._mechlen.size
+            mech = None
+
+        # Now extract the data
+        data = kwargs['payload'][mech_end:]
+
+        return cls(mech, data, **kwargs)
+
+    def __init__(self, mech=None, data=None, **kwargs):
+        """
+        Initialize a ``SASLClientStep`` instance.
+
+        :param mech: The mechanism name.
+        :param data: The initial data for the SASL exchange.
+        :param **kwargs: Additional keyword arguments to use for
+                         initializing the ``Message`` portion.
+        """
+
+        # Initialize the Message
+        super(SASLClientStep, self).__init__(**kwargs)
+
+        # Save the data
+        self.mech = mech
+        self.data = data
+
+    def _encode(self):
+        """
+        Assemble the payload.  The payload will be appended to the carrier
+        protocol header to produce a complete protocol message.
+
+        :returns: The assembled payload.
+        :rtype: ``bytes``
+        """
+
+        # If we have a mechanism, include it
+        if self.mech:
+            return self._mechlen.pack(len(self.mech)) + self.mech + self.data
+
+        # No mechanism name to include
+        return self._mechlen.pack(0) + self.data
+
+    @attrs.FilterAttr
+    def mech(self, mech):
+        """
+        The mechanism name.  This will be a ``bytes`` object or ``None``.
+        """
+
+        # Ensure the mechanism name is bytes or None
+        if mech is not None and not isinstance(mech, bytes):
+            raise ValueError('Mechanism name must be bytes')
+
+        return None if mech is None else mech.upper()
+
+    @attrs.FilterAttr
+    def data(self, data):
+        """
+        The step data.  This will be a ``bytes`` object or ``None``.
+        """
+
+        # Ensure the data is bytes or None
+        if data is not None and not isinstance(data, bytes):
+            raise ValueError('Mechanism data must be bytes')
+
+        return data or b''
+
+
+class SASLServerStep(SASLClientStep):
+    """
+    Represent the server step in a SASL exchange.
+    """
+
+    # Only difference with SASLClientStep is the reply flag
+    default_carrier_flags = 'reply'
+
+
+@Message.register(3)
+def _protocol3(**kwargs):
+    """
+    Decode protocol 3 messages.
+
+    :param carrier_flags: The carrier protocol flags.
+    :param bytes payload: The message payload.
+
+    :returns: An appropriate instance of a subclass of ``Message``
+              representing the protocol 3 message.
+    :rtype: ``Message``
+    """
+
+    # Handle error messages
+    if 'error' in kwargs['carrier_flags']:
+        return SASLError._decode(**kwargs)
+
+    # OK, extract the mechanism name length
+    mechlen = SASLClientStep._mechlen.unpack(
+        kwargs['payload'][:SASLClientStep._mechlen.size]
+    )[0]
+
+    # Handle the special case of mechanism name list
+    if mechlen == 255:
+        cls = (
+            SASLMechanisms if 'reply' in kwargs['carrier_flags']
+            else RequestSASLMechanisms
+        )
+    else:
+        cls = (
+            SASLServerStep if 'reply' in kwargs['carrier_flags']
+            else SASLClientStep
+        )
+
+    # Hand the message off to the class for further decoding
+    return cls._decode(mechlen, **kwargs)
--- tools/hum_proto/tests/unit/test_message.py
+++ tools/hum_proto/tests/unit/test_message.py
@@ -1280,3 +1280,508 @@ class TestProtocol2(object):
             carrier_flags=flags, a=1, b=2, c=3
         )
         assert not mock_StartTLSRequest.called
+
+
+class TestSASLError(object):
+    def test_decode(self, mocker):
+        mock_init = mocker.patch.object(
+            message.SASLError, '__init__', return_value=None
+        )
+        payload = u'This is a test message\u2026'.encode('utf-8')
+
+        result = message.SASLError._decode(
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert isinstance(result, message.SASLError)
+        mock_init.assert_called_once_with(
+            u'This is a test message\u2026',
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+    def test_init(self, mocker):
+        mock_init = mocker.patch.object(
+            message.Message, '__init__', return_value=None
+        )
+
+        result = message.SASLError(u'some message\u2026', a=1, b=2, c=3)
+
+        assert result.msg == u'some message\u2026'
+        mock_init.assert_called_once_with(a=1, b=2, c=3)
+
+    def test_encode(self):
+        msg = u'This is a test message\u2026'
+        obj = message.SASLError(msg)
+
+        result = obj._encode()
+
+        assert result == msg.encode('utf-8')
+
+    def test_msg_str(self):
+        msg = u'This is a test message\u2026'
+
+        result = message.SASLError.msg.prepare('instance', msg)
+
+        assert result == msg
+
+    def test_msg_other(self):
+        msg = 12345
+
+        with pytest.raises(ValueError):
+            message.SASLError.msg.prepare('instance', msg)
+
+
+class TestRequestSASLMechanisms(object):
+    def test_decode(self, mocker):
+        mock_init = mocker.patch.object(
+            message.RequestSASLMechanisms, '__init__', return_value=None
+        )
+
+        result = message.RequestSASLMechanisms._decode(255, a=1, b=2, c=3)
+
+        assert isinstance(result, message.RequestSASLMechanisms)
+        mock_init.assert_called_once_with(a=1, b=2, c=3)
+
+    def test_encode(self):
+        obj = message.RequestSASLMechanisms()
+
+        result = obj._encode()
+
+        assert result == b'\xff'
+
+
+class TestSASLMechanisms(object):
+    def test_decode(self, mocker):
+        mock_init = mocker.patch.object(
+            message.SASLMechanisms, '__init__', return_value=None
+        )
+        payload = b'\xffMECH1 MECH2  MECH3\tMECH4'
+
+        result = message.SASLMechanisms._decode(
+            255,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert isinstance(result, message.SASLMechanisms)
+        mock_init.assert_called_once_with(
+            [b'MECH1', b'MECH2', b'MECH3', b'MECH4'],
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+    def test_init(self, mocker):
+        mock_init = mocker.patch.object(
+            message.Message, '__init__', return_value=None
+        )
+
+        result = message.SASLMechanisms(
+            [b'MECH1', b'MECH2', b'MECH3'], a=1, b=2, c=3
+        )
+
+        assert result.mechs == [b'MECH1', b'MECH2', b'MECH3']
+        mock_init.assert_called_once_with(a=1, b=2, c=3)
+
+    def test_encode(self):
+        obj = message.SASLMechanisms([b'MECH1', b'MECH2', b'MECH3'])
+
+        result = obj._encode()
+
+        assert result == b'\xffMECH1 MECH2 MECH3'
+
+    def test_mechs_base(self):
+        mechs = [b'MECH1', b'MECH2', b'MECH3']
+
+        result = message.SASLMechanisms.mechs.prepare('instance', mechs)
+
+        assert result == mechs
+
+    def test_mechs_upper(self):
+        mechs = [b'mech1', b'mech2', b'mech3']
+
+        result = message.SASLMechanisms.mechs.prepare('instance', mechs)
+
+        assert result == [b'MECH1', b'MECH2', b'MECH3']
+
+    def test_mechs_not_list(self):
+        mechs = 12345
+
+        with pytest.raises(ValueError):
+            message.SASLMechanisms.mechs.prepare('instance', mechs)
+
+    def test_mechs_not_bytes(self):
+        mechs = [b'mech1', b'mech2', 12345]
+
+        with pytest.raises(ValueError):
+            message.SASLMechanisms.mechs.prepare('instance', mechs)
+
+
+class TestSASLClientStep(object):
+    def test_decode_nomech(self, mocker):
+        mock_init = mocker.patch.object(
+            message.SASLClientStep, '__init__', return_value=None
+        )
+        payload = b'\0Step Data'
+
+        result = message.SASLClientStep._decode(
+            0,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert isinstance(result, message.SASLClientStep)
+        mock_init.assert_called_once_with(
+            None, b'Step Data',
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+    def test_decode_withmech(self, mocker):
+        mock_init = mocker.patch.object(
+            message.SASLClientStep, '__init__', return_value=None
+        )
+        payload = b'\4MECHStep Data'
+
+        result = message.SASLClientStep._decode(
+            4,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert isinstance(result, message.SASLClientStep)
+        mock_init.assert_called_once_with(
+            b'MECH', b'Step Data',
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+    def test_init_base(self, mocker):
+        mock_init = mocker.patch.object(
+            message.Message, '__init__', return_value=None
+        )
+
+        result = message.SASLClientStep(a=1, b=2, c=3)
+
+        assert result.mech is None
+        assert result.data == b''
+        mock_init.assert_called_once_with(a=1, b=2, c=3)
+
+    def test_init_alt(self, mocker):
+        mock_init = mocker.patch.object(
+            message.Message, '__init__', return_value=None
+        )
+
+        result = message.SASLClientStep(b'MECH', b'data', a=1, b=2, c=3)
+
+        assert result.mech == b'MECH'
+        assert result.data == b'data'
+        mock_init.assert_called_once_with(a=1, b=2, c=3)
+
+    def test_encode_nomech(self):
+        obj = message.SASLClientStep(data=b'data')
+
+        result = obj._encode()
+
+        assert result == b'\x00data'
+
+    def test_encode_withmech(self):
+        obj = message.SASLClientStep(mech=b'MECH', data=b'data')
+
+        result = obj._encode()
+
+        assert result == b'\x04MECHdata'
+
+    def test_mech_none(self):
+        mech = None
+
+        result = message.SASLClientStep.mech.prepare('instance', mech)
+
+        assert result is None
+
+    def test_mech_bytes(self):
+        mech = b'MECH'
+
+        result = message.SASLClientStep.mech.prepare('instance', mech)
+
+        assert result == b'MECH'
+
+    def test_mech_upper(self):
+        mech = b'mech'
+
+        result = message.SASLClientStep.mech.prepare('instance', mech)
+
+        assert result == b'MECH'
+
+    def test_mech_not_bytes(self):
+        mech = 12345
+
+        with pytest.raises(ValueError):
+            message.SASLClientStep.mech.prepare('instance', mech)
+
+    def test_data_none(self):
+        data = None
+
+        result = message.SASLClientStep.data.prepare('instance', data)
+
+        assert result == b''
+
+    def test_data_bytes(self):
+        data = b'data'
+
+        result = message.SASLClientStep.data.prepare('instance', data)
+
+        assert result == b'data'
+
+    def test_data_not_bytes(self):
+        data = 12345
+
+        with pytest.raises(ValueError):
+            message.SASLClientStep.data.prepare('instance', data)
+
+
+class TestProtocol3(object):
+    def test_error(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset('error')
+        payload = b'Some error message'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_SASLError.return_value
+        mock_SASLError.assert_called_once_with(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+        assert not mock_RequestSASLMechanisms.called
+        assert not mock_SASLMechanisms.called
+        assert not mock_SASLClientStep.called
+        assert not mock_SASLServerStep.called
+
+    def test_mechanism_request(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset()
+        payload = b'\xff'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_RequestSASLMechanisms.return_value
+        assert not mock_SASLError.called
+        mock_RequestSASLMechanisms.assert_called_once_with(
+            255,
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+        assert not mock_SASLMechanisms.called
+        assert not mock_SASLClientStep.called
+        assert not mock_SASLServerStep.called
+
+    def test_mechanism_response(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset('reply')
+        payload = b'\xffmech1 mech2 mech3'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_SASLMechanisms.return_value
+        assert not mock_SASLError.called
+        assert not mock_RequestSASLMechanisms.called
+        mock_SASLMechanisms.assert_called_once_with(
+            255,
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+        assert not mock_SASLClientStep.called
+        assert not mock_SASLServerStep.called
+
+    def test_client_step(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset()
+        payload = b'\x04MECHdata'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_SASLClientStep.return_value
+        assert not mock_SASLError.called
+        assert not mock_RequestSASLMechanisms.called
+        assert not mock_SASLMechanisms.called
+        mock_SASLClientStep.assert_called_once_with(
+            4,
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+        assert not mock_SASLServerStep.called
+
+    def test_client_step_nomech(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset()
+        payload = b'\x00data'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_SASLClientStep.return_value
+        assert not mock_SASLError.called
+        assert not mock_RequestSASLMechanisms.called
+        assert not mock_SASLMechanisms.called
+        mock_SASLClientStep.assert_called_once_with(
+            0,
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+        assert not mock_SASLServerStep.called
+
+    def test_server_step(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset('reply')
+        payload = b'\x04MECHdata'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_SASLServerStep.return_value
+        assert not mock_SASLError.called
+        assert not mock_RequestSASLMechanisms.called
+        assert not mock_SASLMechanisms.called
+        assert not mock_SASLClientStep.called
+        mock_SASLServerStep.assert_called_once_with(
+            4,
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+    def test_server_step_nomech(self, mocker):
+        mock_SASLError = mocker.patch.object(message.SASLError, '_decode')
+        mock_RequestSASLMechanisms = mocker.patch.object(
+            message.RequestSASLMechanisms, '_decode'
+        )
+        mock_SASLMechanisms = mocker.patch.object(
+            message.SASLMechanisms, '_decode'
+        )
+        mock_SASLClientStep = mocker.patch.object(
+            message.SASLClientStep, '_decode'
+        )
+        mock_SASLServerStep = mocker.patch.object(
+            message.SASLServerStep, '_decode'
+        )
+        flags = message.Message.carrier_flags.eset.flagset('reply')
+        payload = b'\x00data'
+
+        result = message._protocol3(
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
+
+        assert result == mock_SASLServerStep.return_value
+        assert not mock_SASLError.called
+        assert not mock_RequestSASLMechanisms.called
+        assert not mock_SASLMechanisms.called
+        assert not mock_SASLClientStep.called
+        mock_SASLServerStep.assert_called_once_with(
+            0,
+            carrier_flags=flags,
+            payload=payload,
+            a=1, b=2, c=3,
+        )
