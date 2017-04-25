43ac8548e4c8c6a3b098ddc0906ad1e748094c2d
Kevin L. Mitchell <klmitch@mit.edu>
2017-04-25 12:13:39 -0500

    Tweak tests
    This change tweaks the test runner a bit to produce more useful
    output.  It also adds an element to the test matrix to test
    what happens with the "--without-openssl" configure flag and
    ensures that build directories are ignored by git.

--- tools/travis-test.sh
+++ tools/travis-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Exit on the first error
-set -ev
+set -e
 
 # Determine the build tree directory
 if [ "${CI}" = true ]; then
@@ -14,34 +14,74 @@ fi
 cd ${build_root}
 build_root=`pwd`
 
+# Set up the banner printer
+sep="======================================================================"
+banner () {
+    leader=$1
+    shift
+    if [ ${leader} -gt 0 ]; then
+	echo ${sep}
+    fi
+    python -c "import textwrap;print(textwrap.fill('$*'))"
+    echo ${sep}
+}
+
+banner 1 "Running tests on ${build_root}"
+
 # Run distcheck
 if [ -z "${HUMBOLDT_TESTENV}" -o "${HUMBOLDT_TESTENV}" = make ]; then
+    args=
+    if [ -n "${DISTCHECK_CONFIGURE_FLAGS}" ]; then
+	args=" with configure flags: ${DISTCHECK_CONFIGURE_FLAGS}"
+    fi
+    banner 0 "Testing Humboldt${args}"
+
     # Begin by generating the autoconf files
-    ./autogen.sh
+    banner 0 "  Running autogen.sh..."
+    ./autogen.sh >/dev/null 2>&1
 
     # Create a build directory and cd there
-    if [ ! -d build ]; then
-	mkdir build
+    banner 0 "  Creating build directory"
+    if [ ! -d travis-build ]; then
+	mkdir travis-build
     fi
-    cd build
+    cd travis-build
 
     # Configure the repository
+    banner 0 "  Configuring the repository"
     ${build_root}/configure
 
     # Run a simple make distcheck
+    banner 1 "  Running \"make distcheck\"${args}"
     make distcheck
 
     # Done; go back to the build root
+    banner 1 "  Done testing Humboldt"
     cd ${build_root}
 fi
 
 # Run tox on our tools
 if [ -z "${HUMBOLDT_TESTENV}" -o "${HUMBOLDT_TESTENV}" = tox ]; then
+    args=
+    if [ -n "${TOXENV}" ]; then
+	args=" on tox environment(s): ${TOXENV}"
+    fi
+    banner 0 "Testing Humboldt Python tools with tox${args}"
+
     # Make sure we have tox
-    pip install tox
+    banner 0 "  Installing tox"
+    pip install tox >/dev/null 2>&1
 
     # Run tox on all the tox.ini files in the tools directory
+    lead=0
     for tox_ini in tools/*/tox.ini; do
+	tool=`dirname ${tox_ini}`
+	tool=`basename ${tool}`
+	banner ${lead} "  Running tox on ${tool}${args}"
 	tox -c ${tox_ini}
+	banner 1 "  Done testing ${tool}"
+	lead=1
     done
 fi
+
+banner 0 "Done testing Humboldt"
